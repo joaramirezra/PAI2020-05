83aa3882b23de78f4ecca5acad1d3565b31e7e21
johan <joaramirezra@unal.edu.co>
2020-04-12 18:49:45 -0500

    version 0.6 Prueba de motor validada

---
+++ Firmware/Programacion/pruebaMotor/pruebaMotor.ino
@@ -0,0 +1,39 @@
+#include <AccelStepper.h>
+
+#define boton 7
+#define enable 8
+#define homing 4
+
+int dato=0;
+bool activateMovement = 0 ;
+AccelStepper stepper = AccelStepper(1, 3, 2); // pin 3 : step, pin 2 : dir ; 1 indica que es driver
+
+void setup() {
+  stepper.setCurrentPosition(0);
+  pinMode(enable, OUTPUT);
+  stepper.setMaxSpeed(3000);     
+  stepper.setAcceleration(1000);
+  digitalWrite(enable, LOW);
+}
+
+void loop() {  
+
+  int velocidad_Subida = 300;
+  int tiempo_meseta = 5000; // milisegundos
+  // inspiracion 
+ 
+  stepper.moveTo(0); 
+  stepper.runToPosition();
+  stepper.setSpeed(velocidad_Subida);
+  stepper.moveTo(500);
+  stepper.runToPosition();
+  // Meseta
+  delay(1500);
+
+  // expiracion
+  stepper.moveTo(0); 
+  stepper.runToPosition();
+  delay(500);
+  
+  
+}
--- Firmware/Respirador_UN/Paciente.h
+++ Firmware/Respirador_UN/Paciente.h
@@ -17,15 +17,15 @@ class Paciente{
   // getters
   byte get_frecuencia(){return frecuencia;}
   byte get_volumen_tidal(){return volumen_tidal;}
-  byte get_presion(){return frecuencia;}
+  byte get_presion(){return presion;}
   // setters
   void set_frecuencia(byte valor){frecuencia = valor;}
   void set_volumen_tidal(byte valor){volumen_tidal = valor;}
-  void set_presion(byte valor){frecuencia = valor;}
+  void set_presion(byte valor){presion = valor;}
 
   void Set_parametros(byte valor , byte valor2, byte valor3){
     frecuencia = valor;
-    volumen_tidal = valor;
-    frecuencia = valor;
+    volumen_tidal = valor2;
+    presion = valor3;
   }
 };
