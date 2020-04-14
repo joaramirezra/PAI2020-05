50d60598da69c3c0a308fcaa486dab4aa37971ca
johan <joaramirezra@unal.edu.co>
2020-04-13 22:04:25 -0500

    v 0.81 Pruebas en maquina

--- Firmware/Programacion/pruebaMotor/pruebaMotor.ino
+++ Firmware/Programacion/pruebaMotor/pruebaMotor.ino
@@ -14,7 +14,6 @@ void setup() {
   // Se configura la posicion actual del motor como cero y se habilita el pin del enable
   stepper.setCurrentPosition(0);
   pinMode(enable, OUTPUT);
-<<<<<<< HEAD
   
   // se configuran los parametros del motor inicial
   stepper.setMaxSpeed(6000);   
@@ -29,39 +28,22 @@ void setup() {
   //Serial.printl
   //Serial.println("velocidad SUBIDA" + String(cicloRespiratorio.velocidad_subida) + "posicion "+String(cicloRespiratorio.posicion_Final_subida));
   //delay(1500);
-=======
-  stepper.setMaxSpeed(12000);     
-  stepper.setAcceleration(1600);
-  digitalWrite(enable, LOW);
-  Serial.begin(9600);
-  delay(1000);
-  
-  int posicion_final= 20000; 
-  int velocidad_Subida = 000;
-  Serial.println("hola Perros");
-  stepper.setSpeed(velocidad_Subida);
-  stepper.moveTo(posicion_final);
-//  stepper.runToPosition();
-  stepper.run();
-  
-  Serial.println("Adios Perros");
->>>>>>> 13423d920c27a60e677d8e9bdc3eab104e8f7881
 }
 
 byte ciclo = 1;
 double tiempo =0;
-void loop() {  
-<<<<<<< HEAD
- // inspiracion 
-  if(ciclo == 1){ // inspiracion 
+void loop() {   
+  // Inicio inspiracion -----------------------------------------------------------------
+  if(ciclo == 1){  
     stepper.moveTo(cicloRespiratorio.posicion_Final_subida-2000);
     stepper.setSpeed(cicloRespiratorio.velocidad_subida);
     ciclo = 2;
-//    Serial.println("inicio : " +String(millis()));
    }
-  if(stepper.distanceToGo()>0 && ciclo == 2 )stepper.runSpeed();
+  // corre a velocidad constante 
+  else if(stepper.distanceToGo()>0 && ciclo == 2 )stepper.runSpeed();
   
-  if(stepper.distanceToGo()<=0 && ciclo == 2 ){// inicio meseta
+  // inicio meseta ----------------------------------------------------------------------
+  else if(stepper.distanceToGo()<=0 && ciclo == 2 ){
     ciclo = 3 ;
     tiempo = millis();
     }
@@ -90,28 +72,4 @@ void loop() {
    }//fin meseta
 
   if( millis()%1000 == 0) Serial.println("tiempo" +String(millis())+ "Posicion " + stepper.currentPosition());
-=======
-<<<<<<< HEAD
-  stepper.run();
-  if(millis()%100 == 0) Serial.println(String(millis())+" "+String(stepper.speed())+" Posicion "+String(stepper.currentPosition()));
-=======
-
-  int velocidad_Subida = 300;
-  int tiempo_meseta = 5000; // milisegundos
-  // inspiracion 
- 
-  stepper.moveTo(0); 
-  stepper.runToPosition();
-  stepper.setSpeed(velocidad_Subida);
-  stepper.moveTo(500);
-  stepper.runToPosition();
-  // Meseta
-  delay(1500);
-
-  // expiracion
-  stepper.moveTo(0); 
-  stepper.runToPosition();
-  delay(500); 
->>>>>>> 64b7da5f9310ab2fbdbfc2758c7b528668dadaa7
->>>>>>> 13423d920c27a60e677d8e9bdc3eab104e8f7881
 }
