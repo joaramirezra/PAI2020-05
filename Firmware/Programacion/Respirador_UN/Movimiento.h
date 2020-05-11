66e1fb708959cea5caa8993c64efb235b0d78de4
JurgenHK <jhkrejcim@unal.edu.co>
2020-05-11 17:47:21 -0500

    V1.2

--- Firmware/Programacion/Respirador_UN/Movimiento.h
+++ Firmware/Programacion/Respirador_UN/Movimiento.h
@@ -7,11 +7,12 @@ class movimiento{
     double aceleracion_subida;           // Calculado Inspiracion
     double posicion_Final_subida;        // Calculado Inspiracion
     double Tiempo_meseta;                // Calculado Meseta
-    double Velocidad_inicial_bajada;     // Calculado espiracion
+    double Velocidad_bajada;     // Calculado espiracion
     double acelaracion_bajada;   // Calculado espiracion
     double modo_motor;
     float coeficiente;
-    double Tiempo_pausa; 
+    double Tiempo_pausa;
+    double offset; 
     
   //Constructor 
   movimiento (){
@@ -22,11 +23,12 @@ class movimiento{
     aceleracion_subida = 0;           
     posicion_Final_subida = 0;        
     Tiempo_meseta = 0;                
-    Velocidad_inicial_bajada = 0;     
+    Velocidad_bajada = 0;     
     acelaracion_bajada = 0;
     modo_motor = 0.5;
     coeficiente = 0;
     Tiempo_pausa = 0;
+    offset = 40; //Es el offset de la mordaza, distancia del home al Ambu
   }
 
   int get_velocidad_subida(){return velocidad_subida;}
@@ -34,26 +36,24 @@ class movimiento{
 
   
   void SetParametros(byte frec, byte vol,byte rela){
+
     //relacion de tiempo sirve para definir subida y bajada 
     double tiempo_Ciclo = 60 /(2*frec+10);
-    // Inspiracion 
-    posicion_Final_subida  = ((vol*100.0)/5.7+40)*(200.0/(8*modo_motor)); // 40 es un offset del tornillo
-    velocidad_subida = ((posicion_Final_subida- (40)*(200.0/(8*modo_motor)))/((tiempo_Ciclo*(1.0/(rela+1)))*0.65));   
-    aceleracion_subida = 0;
-    
-    // se calcula tiempo de meseta 
-    Tiempo_meseta = tiempo_Ciclo*(1.0/(rela+1))*.35;
-    
+
     // Espiracion
     double Tiempo_bajada = tiempo_Ciclo*(double(rela)/(rela+1))*0.7;
     Tiempo_pausa =  tiempo_Ciclo*(double(rela)/(rela+1))*0.3;
-    double Tiempo_inicio =  tiempo_Ciclo*(1.0/(rela+1));
+    double Tiempo_inicio =  tiempo_Ciclo*(1.0/(rela+1))*.7;
     double Tiempo_final =  Tiempo_inicio + Tiempo_bajada;
     double delta_tiempo = Tiempo_inicio  - Tiempo_final;
-  
-    coeficiente = (vol*100.0)/((delta_tiempo )*(delta_tiempo ));  
-    acelaracion_bajada = ((2*coeficiente)/5.7)*(200.0/(8*modo_motor));
-    Velocidad_inicial_bajada = ((2*coeficiente*delta_tiempo)/5.7)*(200.0/(8*modo_motor));
-    //return tiempo_Ciclo*(1.0/(rela+1))*0.65;
+    
+
+    // Inspiracion 
+    posicion_Final_subida  = ((vol*100.0)/5.7+offset); // 40 es un offset del tornillo   
+    aceleracion_subida = ((posicion_Final_subida)/16)/((Tiempo_inicio*Tiempo_inicio)/2);
+    
+    // se calcula tiempo de meseta 
+    Tiempo_meseta = tiempo_Ciclo*(1.0/(rela+1))*.3;
+    Velocidad_bajada = posicion_Final_subida/bajada;
   }  
 };
